de6efee7e8cf2fd52a678a9e1e063e779c7a4c4d
soriya <hor.soriya@gmail.com>
2024-03-16 20:59:19 +0100

    api.py fonctionnelle

--- api.py
+++ api.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 import uvicorn
-import requests
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse, Response
-from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-# ------------------------------------------ Predict ----------------------------------------------------------------
-
-import pickle
-
-# Load the model from file
-with open('model_kmeans', 'rb') as file:
-    model = pickle.load(file)
-
-
-# ------------------------------------------ Model Choice ----------------------------------------------------------------
-
-# Example request model
-class PredictionInput(BaseModel):
-    model_name: str
-    params:list[str]
+import httpx
 
 
 # Instance du moteur de modèles Jinja2 pour la gestion des templates HTML
@@ -38,38 +21,38 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
-    
 
-# Example function to make predictions
-def predict():
-    # Call your machine learning model here and return predictions
-    # Example: 
-    prediction = model.predict()
-    return prediction
+class PredictionInput(BaseModel):
+    model_name: str
+    params:list[str]
 
-@app.get('/choose_model')
-async def run_prediction(model_name: str):
-    # return templates.TemplateResponse("index.html", {"request": request})
+# @app.get('/choose_model_test')
+# async def run_prediction(model_name: str):
+#     if model_name == "Kmeans":
+#         image_path = "plot_kmeans.png"
+#         return FileResponse(image_path, media_type="image/png")
+#     else:
+#         return HTTPException(status_code=404, detail="Model not found")
+
+@app.get('/test') 
+async def test():
+    print("coucou")
+
+@app.post("/choose_model/{model_name}")
+async def run_prediction(n_clusters: int, model_name = str):
     if model_name == "Kmeans":
-        image_path = "plot_kmeans.png"
-        return FileResponse(image_path, media_type="image/png")
-    else:
-        return HTTPException(status_code=404, detail="Model not found")
+    # Send the data to the receiver API
+        async with httpx.AsyncClient() as client:
+            response = await client.post("http://localhost:8001/prediction_kmeans", params={"n_clusters": n_clusters})
 
-# @app.post('/predict_model')
-# async def run_prediction_post(model_data: PredictionInput):
-#     try:
-#         model_name = model_data['model_name']
-#         prediction = predict()
-#         print(f"Prediction : {prediction} effectued with {model_name}")
-#         return JSONResponse(content={"redirect_url": "/plot_results"})
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+            return response.text
 
-
-# @app.get('/plot_results')
-# async def plot_results(request: Request):
-#     return templates.TemplateResponse("plot_results.html", {"request": request})
-
+    if model_name == "agglo":
+    # Send the data to the receiver API
+        async with httpx.AsyncClient() as client:
+            response = await client.post("http://localhost:8001/prediction_agglo", params={"n_clusters": n_clusters})
+            
+            return response.text
+        
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
